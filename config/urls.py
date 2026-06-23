d092738a16216bfbe6964d5b154ce068937f6372
francescociarini <francesco.ciarini@stud.unifi.it>
2026-06-23 23:13:01 +0200

    Serve demo media files on Render

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -11,6 +11,12 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("activities/", include("activities.urls")),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
